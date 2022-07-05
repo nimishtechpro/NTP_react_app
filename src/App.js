2efe01492194904d7674704c84a17ce746da54e6
Nimish <nimishtechpro@gmail.com>
2022-07-04 18:22:11 -0600

    RTU-102 : Passing data from App.js to Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Header from './components/Header'
 function App() {
+  const developerName = 'Nimish'
   const data = 'This is the description area'
   return (
 <div>
-  <Header/>
-<h1>Hello World!</h1>
+  <Header data={developerName}/>
+  <h1>Hello World!</h1>
 <p>{data}</p>
 </div>);
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
-function Header() {
+function Header(props) {
  
-    return (<h1> This is header </h1>)
+    return (<h1> This is header , dev name {props.data} </h1>)
 }
 export default Header
