1cc1e984ba4665dbe4a626eabf2847263e210d21
Nimish <nimishtechpro@gmail.com>
2022-07-04 18:11:52 -0600

    RTU-102 : Created a Header component and included in App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import Header from './components/Header'
 function App() {
   const data = 'This is the description area'
   return (
 <div>
+  <Header/>
 <h1>Hello World!</h1>
 <p>{data}</p>
 </div>);
