803aba5f56d01cd8accc3af016651281e31e23ff
Nimish <nimishtechpro@gmail.com>
2022-07-05 23:12:32 -0600

    RTU-103 : Created a Header component with class

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,9 @@
-function Header(props) {
- 
-    return (<h1> This is header , dev name {props.data} </h1>)
+import {Component} from 'react'
+
+class Header extends Component{
+    render(){
+        return (<h1> This is header , dev name {this.props.data}  </h1>)
+    }
 }
+
 export default Header
